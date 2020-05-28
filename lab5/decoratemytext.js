268c72550dc542e40d31f9ce7f5b87ba36cd7c66
Vo Tam Van <vtvan2k1@gmail.com>
2020-05-27 19:12:51 -0500

    add window.onload

--- lab5/decoratemytext.js
+++ lab5/decoratemytext.js
@@ -7,19 +7,19 @@ function handleDecoration() {
     var style = window.getComputedStyle(sampleTextElem, null).getPropertyValue('font-size');
     var fontSize = parseInt(style) + 2;
     fontSize = Math.min(fontSize, 48);
-    console.log(fontSize);
-    sampleTextElem.style.fontSize = fontSize;
+    // console.log(fontSize);
+    sampleTextElem.style.fontSize = fontSize + "px";
 }
 
 function handleBling() {
+    sayHello();
+
     var blingElem = document.getElementById("bling");
     var sampleTextElem = document.getElementById("sampleText");
     if (blingElem.checked) {
-        sayHello();
         sampleText.style.fontWeight = "bold";
         sampleText.style.color = "green";
         sampleText.style.textDecoration = "underline";
-
     }else {
         sampleText.style.fontWeight = "normal";
         sampleText.style.textDecoration = "none";
@@ -30,3 +30,10 @@ function handleBling() {
 function timer() {
     setInterval(handleDecoration, 500);
 }
+
+function initialization() {
+    document.getElementById("decoration").onclick = timer;
+    document.getElementById("bling").onchange = handleBling;
+}
+
+window.onload = initialization;
