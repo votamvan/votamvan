ecee64107829fd5d41c37e40ea6db676477e43f2
Vo Tam Van <vtvan2k1@gmail.com>
2020-05-28 12:40:00 -0500

    add js function

---
+++ lab6/ascii.js
@@ -0,0 +1,14 @@
+var startTimer;
+function handleStart(){
+    startTimer = setInterval(handleStart, 250);
+}
+
+function handleStart(){
+    clearInterval(startTimer);
+
+}
+
+
+function initialize() {
+    
+}
