087ac48583891f6b2f5e03698f2a710af2d4756d
Vo Tam Van <vtvan2k1@gmail.com>
2020-06-05 00:00:31 -0500

    update hw2

--- lab10/hw2.js
+++ lab10/hw2.js
@@ -1,1 +1,29 @@
+"use strict";
+
+$(function() {
+    var baseUrl = "http://jsonplaceholder.typicode.com/";
+	$('#submit').click(function() {
+		var info = 'users/'+$('#user').val();
+		$.ajax({
+			'url': baseUrl + info,
+			'type': 'GET',
+			'success': ajaxSuccess,
+			'error': ajaxFailure
+		});
+    });
+    function ajaxSuccess(data) {
+        console.log(data);
+        $('#user_name').val(data.username);
+        $('#user_email').val(data.email);
+        var addr = data.address;
+        $('#user_address').val(addr.suite + addr.street + addr.city + addr.zipcode);
+    }
+    function ajaxFailure(xhr, status, exception) {
+        console.log(xhr, status, exception);
+    }
+});
+
+
+
+
 
